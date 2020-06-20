6d999717c98e3285e79fde69d161c61a5e71e2bd
saradzhyanartur <saradzhyanartur@gmail.com>
2020-06-20 18:31:16 -0400

    changed README

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
 # Python-The-Odds-Api-Wrapper
 Python wrapper around The Odds-Api.
-## Description
+## Description & Implementation
 A full Python Wrapper around The Odds-API which provides sports odds data for loads of sports from bookmakers around the world.
-## Implementation
 Get your free API key at https://the-odds-api.com/. 
-The wrapper was tested with
-```
-implementation = CPython
-version_info = 3.7.6.final.0
-```
-The required dependancies are listed in the requirements.txt 
 ## Usage
-Please refer to https://the-odds-api.com/liveapi/guides/v3/ for detailed instructions. The ```demonstration.py``` file contains examples of usage. 
+Please refer to https://the-odds-api.com/liveapi/guides/v3/ for detailed instructions for The Odds-Api. Below is a simple example of the usage.
+```
+from oddsapi import OddsClient
+
+client = OddsClient(api_key="your api key")
+sports = client.retrieve_sports()
+odds = client.retrieve_odds(
+    sport_key='americanfootball_ncaaf',
+    region='us',
+    mkt='spreads'
+)
+print(f"Requests Remaining: {client.requests_remaining}")
+print(f"Requests Used: {client.requests_used}")
+```
--- setup.py
+++ setup.py
@@ -1,30 +1,22 @@
-from distutils.core import setup
+import setuptools
 
-setup(
-  name = 'oddsapi',        
-  packages = ['oddsapi'],  
-  version = '0.1', 
-  license='MIT',       
-  description = 'Python wrapper around The Odds-Api.',  
-  author = 'Artur Saradzhyan',                  
-  author_email = 'saradzhyanartur@gmail.com',    
-  url = 'https://github.com/saradzhyanartur/oddsapi', 
-  download_url = 'https://github.com/saradzhyanartur/oddsapi/archive/v_01.tar.gz',   
-  keywords = ['The Odds-Api', 'The Odds-API', 'Sports', 'Betting', 'Odds'], 
-  install_requires=[        
-          'urllib3==1.25.9',
-          'certifi==2020.4.5.1',
-      ],
-  classifiers=[
-    'Development Status :: 5 - Production/Stable',   
-    'Intended Audience :: Developers',     
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License', 
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8'
-  ],
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="oddsapi", 
+    version="0.3.1",
+    author="Artur Saradzhyan",
+    author_email="saradzhyanartur@gmail.com",
+    description="Python Wrapper around The Odds-Api",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/saradzhyanartur/oddsapi",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
